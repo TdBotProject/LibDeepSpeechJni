23ea6f4b63624f31c3ada075f2de50fc500e3377
世界 <i@nekox.me>
2021-01-23 20:46:42 +0800

    Add action build

--- linux_build.sh
+++ linux_build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 [ -z "$ARCH" ] && ARCH="amd64"
+[ -z "$TYPE" ] && TYPE="cpu"
 [ -z "$VERSION" ] && VERSION="0.9.3"
-REQUIRED_PACKAGES="wget xz-utils default-jdk-headless"
+REQUIRED_PACKAGES="wget xz-utils default-jdk cmake g++"
 
-sudo apt-get install -y $REQUIRED_PACKAGES || apt-get install $REQUIRED_PACKAGES || exit 1
-wget -O native-client.tar.xz "https://github.com/mozilla/DeepSpeech/releases/download/v${VERSION}/native_client.${ARCH}.cpu.linux.tar.xz" || exit 1
+sudo apt-get update || apt-get update
+sudo apt-get install -y $REQUIRED_PACKAGES || apt-get install -y $REQUIRED_PACKAGES || exit 1
+wget -O native-client.tar.xz "https://github.com/mozilla/DeepSpeech/releases/download/v${VERSION}/native_client.${ARCH}.${TYPE}.linux.tar.xz" || exit 1
 tar -xf native-client.tar.xz
 rm native-client.tar.xz
 
